7fc3abdb8275b23d928e458b913ed7194be855cf
wanho1108 <wanho1108@gmail.com>
2020-01-08 11:21:56 +0900

    #3.10 follow unfollow Resolver

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,6 +7,14 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
+        await prisma.updateUser({
+          whre: {
+            id: user.id
+          },
+          data: {
+            loginSecret: ''
+          }
+        });
         return generateToken(user.id);
       } else {
         throw Error('Wrong email/secret combination');
