f8eee0cd0624f6dc062b5a7dfa172e6ffbd9841b
wanho1108 <wanho1108@gmail.com>
2020-01-08 14:24:28 +0900

    #3.12 me Resolver + Prisma's Limitations

---
+++ src/api/User/me/me.js
@@ -0,0 +1,18 @@
+import { prisma } from "../../../../generated/prisma-client";
+// import { USER_FRAGMENT } from '../../../fragment';
+
+export default {
+  Query: {
+    me: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { id } = request.user;
+      // return prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
+      const user = await prisma.user({ id });
+      const posts = await prisma.user({ id }).posts();
+      return {
+        user,
+        posts
+      };
+    }
+  }
+}
