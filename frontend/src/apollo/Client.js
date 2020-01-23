6dda6f9ba030eef28f3d354341d5bc2b1639b738
wanho1108 <wanho1108@gmail.com>
2020-01-23 15:33:52 +0900

    #4.3 Apollo Client

--- frontend/src/apollo/Client.js
+++ frontend/src/apollo/Client.js
@@ -2,7 +2,7 @@ import AplloClient from 'apollo-boost';
 import { defaults, resolvers } from './LocalState';
 
 export default new AplloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://localhost:4000',
   clientState: {
     defaults,
     resolvers
