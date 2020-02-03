709181bc0a5885a043eecdfe90c8ace57e0f9183
wanho1108 <wanho1108@gmail.com>
2020-02-03 17:53:41 +0900

    #8.0 Search Screen Queries 진행중

--- frontend/src/routes/Search.js
+++ frontend/src/routes/Search.js
@@ -1,1 +1,11 @@
-export default () => 'Search';
+import React, { useCallback } from 'react';
+import { withRouter } from 'react-router-dom';
+
+export default withRouter(({location: { search }}) => {
+  const searchTerm = search.split('=')[1];
+
+  if (searchTerm == undefined) {
+
+  }
+  return null;
+});
