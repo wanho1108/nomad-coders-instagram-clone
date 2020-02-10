021d791ccfec2691259583f652e1f57d17649f11
wanho1108 <wanho1108@gmail.com>
2020-02-10 14:34:36 +0900

    #8.2 Search Screen UI Part Two

--- frontend/src/components/UserCard.js
+++ frontend/src/components/UserCard.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import Avartar from './Avatar';
+import Avatar from './Avatar';
 import FatText from './FatText';
 import Button from './Button';
+import { Link } from 'react-router-dom';
 
-const Card = styled.div``;
+const Card = styled.div`
+  ${props => props.theme.whiteBox}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+
+const EAvatar = styled(Avatar)`
+  margin-bottom: 15px;
+`
+
+const ELink = styled(Link)`
+  color: inherit;
+  margin-bottom: 10px;
+`;
 
 const UserCard = ({ username, isFollowing, url, isSelf }) => {
   return (
     <Card>
-      <Avartar url={url} />
-      <FatText text={username} />
+      <EAvatar url={url} size={'md'} />
+      <ELink to={`/${username}`}>
+        <FatText text={username} />
+      </ELink>
       {!isSelf && <Button text={isFollowing ? 'Unfollow' : 'Follow'} />}
     </Card>
   );
